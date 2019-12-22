a026c581d6b1d1d96d71e579c08202865d5273f9
tkdhss111 <tkdhss111@gmail.com>
2019-12-22 12:03:58 +0900

    xrdp

--- taketa_install_softwares.sh
+++ taketa_install_softwares.sh
@@ -7,12 +7,13 @@ apt install baobab, gnumeric, htop, meld, gparted, apcalc -y
 apt remove --purge Cheese -y
 
 # Remote desktop
-apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
-apt install xrdp -y
-systemctl status xrdystemctl status xrdp
-adduser xrdp ssl-cert
-cat <<EOF>> /etc/xrdp/xrdp.ini
-exec startxfce4
-EOF
-ufw allow 3389
-systemctl restart xrdp
+apt remove --purge xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils xrdp -y
+#apt install xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils -y
+#apt install xrdp -y
+#systemctl status xrdystemctl status xrdp
+#adduser xrdp ssl-cert
+#cat <<EOF>> /etc/xrdp/xrdp.ini
+#exec startxfce4
+#EOF
+#ufw allow 3389
+#systemctl restart xrdp
